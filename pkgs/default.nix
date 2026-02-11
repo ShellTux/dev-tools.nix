5c39180293d5a63900bdd57bdd4d116f3df06f14
ShellTux <115948079+ShellTux@users.noreply.github.com>
2026-02-11 00:52:50 +0000

    nixvim

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,28 @@
 {
   pkgs,
+  inputs',
+  system,
   ...
 }:
 let
   inherit (pkgs) callPackage;
+  nixvim' = inputs'.nixvim.legacyPackages;
+  nixvimModule = {
+    inherit system pkgs;
+    module = import ./nixvim;
+    extraSpecialArgs = {
+      inherit pkgs;
+      leader-key = " ";
+      noice.enable = true;
+    };
+  };
 in
 {
-  packages = {
+  packages = rec {
     bat = callPackage ./bat { };
     default = pkgs.hello;
+    neovim = nixvim;
+    nixvim = nixvim'.makeNixvimWithModule nixvimModule;
+    nvim = nixvim;
   };
 }
